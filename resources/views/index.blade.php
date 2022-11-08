6c4cfa70a58858dec197fb4e67be5db339807622
Ian <ian.fang@quad.tw>
2022-11-08 11:51:36 +0800

    use blade layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,18 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="https://code.jquery.com/jquery-3.6.1.min.js"
-        integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <title>index</title>
-</head>
-
-<body>
+@extends('layouts.layout')
+@section('title', 'index')
+@section('content')
     <div class="container">
         <div class="col d-flex justify-content-center m-5">
             <span class="h3">hello, <a href="/user/{{ $id }}">{{ $name }}</a>.</span>
@@ -21,37 +9,37 @@
             <button type="button" class="btn btn-primary" id="btn-logout">log out</button>
         </div>
     </div>
-</body>
-<script>
-    $(function() {
-        $("#btn-logout").on("click", function(){
-            const logout = new Promise((res, rej) => {
-                $.ajax({
-                    type: "POST",
-                    url: "/logOut",
-                    dataType: "json",
-                    success: function(response) {
-                        res(response);
-                    },
-                    error: function(error) {
-                        rej(error);
-                    }
+@endsection
+@section('script')
+    <script>
+        $(function() {
+            $("#btn-logout").on("click", function() {
+                const logout = new Promise((res, rej) => {
+                    $.ajax({
+                        type: "POST",
+                        url: "/logOut",
+                        dataType: "json",
+                        success: function(response) {
+                            res(response);
+                        },
+                        error: function(error) {
+                            rej(error);
+                        }
+                    });
                 });
-            });
-            (async () => {
-                try {
-                    const result = await logout;
-                    if (!result.status) {
-                        $("#error-msg").text(result.msg);
-                        return;
+                (async () => {
+                    try {
+                        const result = await logout;
+                        if (!result.status) {
+                            $("#error-msg").text(result.msg);
+                            return;
+                        }
+                        window.location.href = window.location.href;
+                    } catch (e) {
+
                     }
-                    window.location.href = window.location.href;
-                } catch (e) {
-
-                }
-            })();
-        })
-    });
-</script>
-
-</html>
+                })();
+            })
+        });
+    </script>
+@endsection
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,18 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="https://code.jquery.com/jquery-3.6.1.min.js"
-        integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <title>login</title>
-</head>
-
-<body>
+@extends('layouts.layout')
+@section('title', 'login')
+@section('content')
     <div class="container my-5">
         <form id="form-login">
             <div class="mb-3">
@@ -30,54 +18,54 @@
             </div>
         </form>
     </div>
-</body>
-<script>
-    $(function() {
-        $("#btn-login").on("click", function() {
+@endsection
+@section('script')
+    <script>
+        $(function() {
+            $("#btn-login").on("click", function() {
 
-            $("#error-msg").text('');
+                $("#error-msg").text('');
 
-            if(!$("#email").val() || !$("#password").val()){
-                return $("#error-msg").text('please input email/password.');
-            }
-
-            const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-            if(!emailRegex.test($("#email").val())){
-                return $("#error-msg").text('invalid email address.');
-            }
-
-            const form = $("#form-login")[0];
-            const formdata = new FormData(form);
-            const login = new Promise((res, rej) => {
-                $.ajax({
-                    type: "POST",
-                    url: "/logIn",
-                    dataType: "json",
-                    contentType: false, //required
-                    processData: false, // required
-                    data: formdata,
-                    success: function(response) {
-                        res(response);
-                    },
-                    error: function(error) {
-                        rej(error);
-                    }
-                });
-            });
-            (async () => {
-                try {
-                    const result = await login;
-                    if (!result.status) {
-                        $("#error-msg").text(result.msg);
-                        return;
-                    }
-                    window.location.href = window.location.href;
-                } catch (e) {
-
+                if (!$("#email").val() || !$("#password").val()) {
+                    return $("#error-msg").text('please input email/password.');
                 }
-            })();
-        })
-    });
-</script>
 
-</html>
+                const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+                if (!emailRegex.test($("#email").val())) {
+                    return $("#error-msg").text('invalid email address.');
+                }
+
+                const form = $("#form-login")[0];
+                const formdata = new FormData(form);
+                const login = new Promise((res, rej) => {
+                    $.ajax({
+                        type: "POST",
+                        url: "/logIn",
+                        dataType: "json",
+                        contentType: false, //required
+                        processData: false, // required
+                        data: formdata,
+                        success: function(response) {
+                            res(response);
+                        },
+                        error: function(error) {
+                            rej(error);
+                        }
+                    });
+                });
+                (async () => {
+                    try {
+                        const result = await login;
+                        if (!result.status) {
+                            $("#error-msg").text(result.msg);
+                            return;
+                        }
+                        window.location.href = window.location.href;
+                    } catch (e) {
+
+                    }
+                })();
+            })
+        });
+    </script>
+@endsection
